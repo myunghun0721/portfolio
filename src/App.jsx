a2dee086cc6a7a1c3bc3e788f54a2df4c2129297
myunghun choi <myunghun0721@gmail.com>
2025-05-05 18:59:57 -0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
   return (
@@ -10,20 +11,7 @@ const App = () => {
         v7_relativeSplatPath: true,
       }}
     >
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
+      <Navbar />
     </BrowserRouter>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server:{
+    watch:{
+      usePolling: true
+    }
+  },
   plugins: [react()],
-  base: "/portfolio"
+  base: "/portfolio",
 });
